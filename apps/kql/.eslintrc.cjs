4567bd6d124372186730f5f1ccefe0f45ba62e06
Esteban Victorio <34945296+EstebanVictorio@users.noreply.github.com>
2023-09-08 18:31:54 -0700

    feat: add husky, lint configs and lint files

--- apps/kql/.eslintrc.cjs
+++ apps/kql/.eslintrc.cjs
@@ -2,22 +2,6 @@
  * @type {import("eslint").Linter.Config} config
  */
 const config = {
-  root: true,
-  env: { es2020: true },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  rules: {
-    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    
-  },
-  overrides: [
-    {
-      rules: {
-        '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-      }
-    }
-  ]
 }
 
 module.exports = config
--- apps/kql/src/resolvers/Query/hello.ts
+++ apps/kql/src/resolvers/Query/hello.ts
@@ -1,5 +1,5 @@
 import { GraphQLResolveInfo } from "graphql"
 
-export const hello = (_: unknown, __: unknown, ___: unknown, ____: GraphQLResolveInfo) => {
+export const hello = (_a: unknown, __b: unknown, ___c: unknown, ____: GraphQLResolveInfo) => {
   return "Hello world from GraphQL!"
 }
